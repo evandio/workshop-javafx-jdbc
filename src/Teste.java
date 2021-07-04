06243946565c7e07481e1a6f9e5c4e5f226b5c77
Evandio Pereira <evandiopereira@gmail.com>
2021-07-03 21:18:21 -0300

    Initializing action as parameter

---
+++ src/Teste.java
@@ -0,0 +1,156 @@
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author evand
+ */
+public class Teste {
+
+    private static String driver;
+    private static String url;
+    private static String usuario;
+    private static String senha;
+
+    /**
+     *
+     * @throws NutricaoException
+     */
+    private static void init() throws Exception {
+        Properties properties = new Properties();
+        FileInputStream arquivoPropriedade = null;
+
+        try {
+            arquivoPropriedade = new FileInputStream("dbConexao.properties");
+            properties.load(arquivoPropriedade);
+
+            driver = properties.getProperty("jdbc.driver");
+            url = properties.getProperty("jdbc.url");
+            usuario = properties.getProperty("jdbc.username");
+            senha = properties.getProperty("jdbc.password");
+
+            Class.forName(driver);
+
+        } catch (IOException ex) { //Fim do try
+            ex.printStackTrace();
+            StringBuffer mensagem = new StringBuffer("Não foi possível conectar com o Banco de Dados!");
+            mensagem.append("\nMotivo: " + ex.getMessage());
+            throw new Exception(mensagem.toString());
+
+        } catch (ClassNotFoundException ex) {//Fim do catch
+            ex.printStackTrace();
+            StringBuffer mensagem = new StringBuffer("Não foi possível conectar com o Banco de Dados!");
+            mensagem.append("\nMotivo: " + ex.getMessage());
+            throw new Exception(mensagem.toString());
+        } //Fim do catch
+    }
+
+    /**
+     *
+     * @return Connection
+     * @throws br.gov.ce.cidh.excecao.SisInfoCidhException
+     */
+    public static Connection getConexao() throws Exception {
+        try {
+            init();
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException ex) {//Fim try
+            ex.printStackTrace();
+            StringBuffer mensagem = new StringBuffer("Não foi possível estabelecer conexao com o banco de dados!");
+            mensagem.append("\nMotivo: " + ex.getMessage());
+            throw new Exception(mensagem.toString());
+        }//Fim do catch
+    }
+
+    /**
+     *
+     * @param conn
+     * @param stmt
+     * @param rs
+     * @throws br.gov.ce.cidh.excecao.SisInfoCidhException
+     */
+    public static void closeConexao(Connection conn, PreparedStatement stmt, ResultSet rs) throws Exception {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) { //fim do try
+            StringBuffer mensagem = new StringBuffer("Não foi possível finalizar a conexão! ");
+            mensagem.append("\nMotivo: " + ex.getMessage());
+            throw new Exception(mensagem.toString());
+        } //fim do catch
+    }
+
+    /**
+     *
+     * @param conn
+     * @param stmt
+     * @param rs
+     * @throws br.gov.ce.cidh.excecao.SisInfoCidhException
+     */
+    public static void closeConexao(Connection conn, Statement stmt, ResultSet rs) throws Exception {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {//Fim do try
+            StringBuffer mensagem = new StringBuffer("Não foi possível finalizar a conexão!");
+            mensagem.append("\nMotivo: " + ex.getMessage());
+            throw new Exception(mensagem.toString());
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
+
+        String sql = "select * from department";
+
+        try {
+            conn = Teste.getConexao();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println("Name: " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
--- src/main/model/entities/Department.java
+++ src/main/model/entities/Department.java
@@ -6,6 +6,7 @@
 package main.model.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -48,6 +49,13 @@ public class Department implements Serializable {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
@@ -58,24 +66,13 @@ public class Department implements Serializable {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        Department other = (Department) obj;
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        } else if (!id.equals(other.id)) {
+        final Department other = (Department) obj;
+        if (!Objects.equals(this.id, other.id)) {
             return false;
         }
-        return false; 
-
+        return true;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
+    
 
 }
